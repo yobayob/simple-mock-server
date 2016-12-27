c376dc673bebfc63a951d8ea0018b6fd26f2d3ef
ekonechnyi <yobayob@yandex.ru>
2016-12-27 17:49:22 +0300

    fix

--- projects/family/templatetags/my_custom_tags.py
+++
@@ -1,9 +0,0 @@
-from django.forms import CheckboxInput
-from django import template
-
-register = template.Library()
-
-def is_checkbox(field):
-	return field.field.widget.__class__.__name__ == CheckboxInput().__class__.__name__
-
-register.filter('is_checkbox', is_checkbox)
